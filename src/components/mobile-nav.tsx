510043047bf4e0f0b9bf978b54de8c909b08ac28
Myles Jefferson <99mylesjefferson@gmail.com>
2024-10-27 15:32:30 -0600

    fix(mobile-nav): display theme toggle on mobile devices

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -6,34 +6,37 @@ import { Button } from "@/components/ui/button";
 import { Menu as MenuIcon } from "lucide-react";
 import { links } from "@/utils/constants";
 import Link from "next/link";
+import { ThemeToggle } from "./theme-toggle";
 export const MobileNav = () => {
 	const [open, setOpen] = useState(false);
 
 	return (
-		<Sheet open={open} onOpenChange={setOpen}>
-			{/* This button will trigger open the mobile sheet menu */}
-			<SheetTrigger asChild>
-				<Button variant="ghost" size="icon" className="md:hidden">
-					<MenuIcon />
-				</Button>
-			</SheetTrigger>
-
-			<SheetContent side="left">
-				<div className="flex flex-col items-start">
-					{links.map(({ name, href }) => (
-						<Button
-							key={name}
-							variant="link"
-							onClick={() => {
-								setOpen(false);
-							}}
-							asChild
-						>
-							<Link href={href}>{name}</Link>
-						</Button>
-					))}
-				</div>
-			</SheetContent>
-		</Sheet>
+		<div className="flex md:hidden justify-between items-center w-full">
+			<Sheet open={open} onOpenChange={setOpen}>
+				{/* This button will trigger open the mobile sheet menu */}
+				<SheetTrigger asChild>
+					<Button variant="ghost" size="icon" className="md:hidden">
+						<MenuIcon />
+					</Button>
+				</SheetTrigger>
+				<SheetContent side="left">
+					<div className="flex flex-col items-start">
+						{links.map(({ name, href }) => (
+							<Button
+								key={name}
+								variant="link"
+								onClick={() => {
+									setOpen(false);
+								}}
+								asChild
+							>
+								<Link href={href}>{name}</Link>
+							</Button>
+						))}
+					</div>
+				</SheetContent>
+			</Sheet>
+			<ThemeToggle />
+		</div>
 	);
 };
